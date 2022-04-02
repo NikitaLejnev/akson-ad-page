ee726075561a85f2853e41f7dc019704e68848f9
Nikita Lezhnev <catassasin331@gmail.com>
2022-04-02 12:56:37 +0300

    Prototype city select popup

---
+++ src/components/CitySelectPopup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="popup">
+    <b class="popup__heading">Ваш город Кострома</b>
+  </div>
+</template>
+
+<style>
+.popup {
+  position: absolute;
+  top: calc(100% + 2px);
+  left: 0;
+  display: block;
+  width: 640px;
+  padding: 32px 24px;
+  border-radius: 0 0 8px 8px;
+  box-sizing: border-box;
+  box-shadow: 0 6px 9px rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+  z-index: 110;
+}
+</style>
