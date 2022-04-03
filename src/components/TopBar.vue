4bd4e5a686680bb20e2da720ba459fbd961ea886
Nikita Lezhnev <catassasin331@gmail.com>
2022-04-03 16:42:49 +0300

    Move geo icon to separate component

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import store from "../store";
 import { selectedCityName } from "@/utils";
 import CitySelectPopup from "./CitySelectPopup.vue";
+import TopBarGeoIcon from "./TopBarGeoIcon.vue";
 
 const state = ref({
   citySelectOpened: false,
@@ -23,21 +24,7 @@ const openSelectCity = () => {
             @click="openSelectCity"
             class="top-bar__city-select"
           >
-            <i class="top-bar__icon--geo">
-              <svg
-                width="20"
-                height="20"
-                viewBox="0 0 20 20"
-                fill="none"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M10 17.5C10 17.5 16.5217 13.4091 16.5217 8.63636C16.5217 5.24734 13.6019 2.5 10 2.5C6.39815 2.5 3.47827 5.24734 3.47827 8.63636C3.47827 13.4091 10 17.5 10 17.5ZM13 9C13 10.6569 11.6569 12 10 12C8.34315 12 7 10.6569 7 9C7 7.34315 8.34315 6 10 6C11.6569 6 13 7.34315 13 9Z"
-                  fill="#fff"
-                ></path>
-              </svg>
-            </i>
+            <top-bar-geo-icon />
             <span class="top-bar__city-select-text">{{
               selectedCityName
             }}</span>
@@ -81,14 +68,6 @@ const openSelectCity = () => {
 .top-bar__city-select {
   display: inline-flex;
   cursor: pointer;
-}
-
-.top-bar__icon--geo {
-  position: relative;
-  margin-right: 1rem;
-  width: 1rem;
-  height: 1rem;
-  left: 0.5rem;
 }
 
 .top-bar__city-select-text {
